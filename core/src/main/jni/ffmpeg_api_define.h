a80584e3a46a663c9a84e8a1ef151a925c2cbf04
joffy <joffychim@163.com>
2018-02-09 09:59:46 +0800

    找不到头文件问题

--- core/src/main/jni/ffmpeg_api_define.h
+++ core/src/main/jni/ffmpeg_api_define.h
@@ -20,11 +20,11 @@
 #define LIBRARY_FUNC(RETURN_TYPE, NAME, ...) \
   extern "C" { \
   JNIEXPORT RETURN_TYPE \
-    Java_com_google_android_exoplayer2_ext_ffmpeg_ffmpegLibrary_ ## NAME \
+    Java_com_google_android_exoplayer2_ext_ffmpeg_FFmpegLibrary_ ## NAME \
       (JNIEnv* env, jobject thiz, ##__VA_ARGS__);\
   } \
   JNIEXPORT RETURN_TYPE \
-    Java_com_google_android_exoplayer2_ext_ffmpeg_ffmpegLibrary_ ## NAME \
+    Java_com_google_android_exoplayer2_ext_ffmpeg_FFmpegLibrary_ ## NAME \
       (JNIEnv* env, jobject thiz, ##__VA_ARGS__)\
 
 #endif //MQPLAYER_FFMPEG_LIB_DEFINE_H
