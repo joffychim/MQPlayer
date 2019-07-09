cdb8a6c12258dcff22ce534287ad4fb62202bcac
joffychim <zhanzenghui@bytedance.com>
2019-07-09 20:26:19 +0800

    关联视频文件

--- app/src/main/java/com/moqan/mqplayer/demo/MainActivity.kt
+++ app/src/main/java/com/moqan/mqplayer/demo/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.Surface
 import android.view.SurfaceView
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.moqan.mqplayer.IMediaPlayer
 import com.moqan.mqplayer.MQMediaPlayer
@@ -20,60 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        player = MQMediaPlayer(this)
-
-        val surfaceView = findViewById<SurfaceView>(R.id.sv_video)
-        player.seekTo(1422580)
-
-        player.setDataSource(Uri.fromFile(File("/sdcard/lemon/123.mkv")))
-
-        player.setDisplay(surfaceView)
-
-        player.addListener(object : IMediaPlayer.EventListener {
-            override fun onPlaybackStateChanged(playingWhenReady: Boolean, playbackState: Int) {
-                super.onPlaybackStateChanged(playingWhenReady, playbackState)
-                Log.i(TAG, "onSeekComplete:$playingWhenReady,$playbackState")
-            }
-
-            override fun onSeekComplete(positionAfterSeek: Long) {
-                super.onSeekComplete(positionAfterSeek)
-                Log.i(TAG, "onSeekComplete:$positionAfterSeek")
-            }
-
-            override fun onRenderedFirstFrame() {
-                super.onRenderedFirstFrame()
-                Log.i(TAG, "onRenderedFirstFrame")
-            }
-
-            override fun onVideoSizeChanged(width: Int, height: Int, unappliedRotationDegrees: Int, pixelWidthHeightRatio: Float) {
-                super.onVideoSizeChanged(width, height, unappliedRotationDegrees, pixelWidthHeightRatio)
-                Log.i(TAG, "onVideoSizeChanged:($width, $height)")
-            }
-
-            override fun onDurationChanged(duration: Long) {
-                Log.i(TAG, "duration：${player.getDuration()}")
-            }
-        })
-
-        findViewById<Button>(R.id.btn_pause).setOnClickListener {
-            player.setPlayWhenReady(false)
-
-        }
-
-        findViewById<Button>(R.id.btn_resume).setOnClickListener {
-            player.setPlayWhenReady(true)
-        }
-
-        findViewById<Button>(R.id.btn_reset).setOnClickListener {
-            player.reset()
-        }
-
-        findViewById<Button>(R.id.btn_stop).setOnClickListener {
-            player.stop()
-        }
-
-        findViewById<Button>(R.id.btn_prepare).setOnClickListener {
-            player.prepare()
-        }
+        Toast.makeText(this, "文件浏览器播中放视频时，请选择用MQPlayer打开", Toast.LENGTH_LONG).show()
+        finish()
     }
 }
