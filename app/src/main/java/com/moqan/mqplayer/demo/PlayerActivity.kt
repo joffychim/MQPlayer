4fa30480a354cc0b3785ff8e0c7f272c3f929cd5
joffychim <zhanzenghui@bytedance.com>
2019-07-11 13:54:51 +0800

    demo加上生命周期调用

--- app/src/main/java/com/moqan/mqplayer/demo/PlayerActivity.kt
+++ app/src/main/java/com/moqan/mqplayer/demo/PlayerActivity.kt
@@ -9,20 +9,40 @@ import com.google.android.exoplayer2.ext.ffmpeg.video.FrameScaleType
 import com.moqan.mqplayer.MQMediaPlayer
 
 class PlayerActivity : AppCompatActivity() {
+    private lateinit var player: MQMediaPlayer
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
         val surfaceView: SurfaceView = findViewById(R.id.sf_holder)
-        val player = MQMediaPlayer(this)
+        player = MQMediaPlayer(this)
         player.setBackgroundColor(Color.argb(255, 255, 255, 255))
         player.setScaleType(FrameScaleType.FIT_Y)
         player.setLooping(true)
 
-        player.setPlayWhenReady(true)
         player.setDisplay(surfaceView)
         player.setDataSource(Uri.parse(intent.dataString))
         player.prepare()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        player.setPlayWhenReady(true)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        player.setPlayWhenReady(false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        player.release()
     }
 }
